91243383499439dc7c4507351611d4a5aa956b7e
Chribtoper <xcstolfax@gmail.com>
2018-09-12 20:44:28 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+
+  def keys_of(*arguments)
+    arr = []
+      self.each do |k, v|
+        arguments.each do |arg|
+          if arg == v
+            arr.push(k)
+          end
+        end
+      end
+      arr
   end
+
 end
