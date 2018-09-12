d5b9353c1f227087a1a7e3b935597ed67cefa400
Albert Chun <albert.s.chun@gmail.com>
2018-09-12 16:28:31 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    array = []
+    self.each do |key, value|
+      # Fails with Hash.each, why is it self?
+      arguments.each do |x|
+        if x == value
+          array << key
+        end
+      end
+    end
+    return array
   end
 end
